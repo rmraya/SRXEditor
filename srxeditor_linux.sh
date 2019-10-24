78fc8bfc73ee710a534545e5dc24fcb09fb2515c
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-10-24 20:49:32 -0300

    fixed launcher

--- srxeditor_linux.sh
+++ srxeditor_linux.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "$0")/"
 
-CLASSPATH="lib/srxeditor.jar:lib/mapdb.jar:lib/openxliff.jar:lib/dtd.jar:lib/json.jar:lib/jsoup-1.11.3,jar:lib/gtk64/swt.jar""
+export CLASSPATH="lib/srxeditor.jar:lib/mapdb.jar:lib/openxliff.jar:lib/dtd.jar:lib/json.jar:lib/jsoup-1.11.3,jar:lib/gtk64/swt.jar"
 
 java  -cp ${CLASSPATH} com.maxprograms.srxeditor.SRXEditor &

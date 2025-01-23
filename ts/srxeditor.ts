fe8ba7d4449518575b7f1fe59c174666f451e58a
Rodolfo M. Raya <rmraya@maxprograms.com>
2025-01-23 08:30:57 -0300

    Started TypeScript code

---
+++ ts/srxeditor.ts
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2020 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
+
+class SRXEditor {
+    constructor() {
+        console.log("SRXEditor constructor");
+    }
+}

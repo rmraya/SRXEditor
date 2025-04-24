48565e129cadfd47344f57a5a68561ac1e59ba10
Rodolfo M. Raya <rmraya@maxprograms.com>
2025-04-24 06:14:35 -0300

    Removed Java launchers

--- srxeditor_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")/"
-
-export CLASSPATH="lib/srxeditor.jar:lib/mapdb.jar:lib/openxliff.jar:lib/dtd.jar:lib/json.jar:lib/jsoup-1.11.3,jar:lib/gtk64/swt.jar"
-
-java  -cp ${CLASSPATH} com.maxprograms.srxeditor.SRXEditor &
